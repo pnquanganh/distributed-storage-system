dc6bf1d8d73a892fd8a1178e8a679c7ebb65d2b2
pnquanganh1986@gmail.com <pnquanganh1986@gmail.com@81b93509-0b8c-f939-ac7f-ba0bbaeee7bc>
2012-10-01 07:23:34 +0000

    Add interface between Client and Master

---
+++ trunk/Client/CE7490_Client/src/client_master_interface/client_master_interface.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client_master_interface;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ *
+ * @author pham0071
+ */
+
+
+public interface client_master_interface extends Remote {
+    Writing_request_result get_writing_slaves() throws RemoteException;
+    Reading_request_result get_reading_slaves(String filename) throws RemoteException;    
+}
