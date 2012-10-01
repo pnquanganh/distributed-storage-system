b4198257ef6d96a51217a0aa7cd729195476e6fe
pnquanganh1986@gmail.com <pnquanganh1986@gmail.com@81b93509-0b8c-f939-ac7f-ba0bbaeee7bc>
2012-10-01 07:27:25 +0000

    Add interface between Client and Slaves

---
+++ Client/CE7490_Client/src/client_slave_interface/client_slave_interface.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client_slave_interface;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+/**
+ *
+ * @author pham0071
+ */
+public interface client_slave_interface extends Remote {
+    
+}
