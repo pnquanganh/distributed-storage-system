51c7511c417741d71e4a1a56833f65ed657a6cd5
pnquanganh1986@gmail.com <pnquanganh1986@gmail.com@81b93509-0b8c-f939-ac7f-ba0bbaeee7bc>
2012-10-01 08:06:10 +0000

    Add information of filename and filesize when request writing operation to the Master

--- Client/CE7490_Client/src/client_master_interface/client_master_interface.java
+++ Client/CE7490_Client/src/client_master_interface/client_master_interface.java
@@ -14,6 +14,6 @@ import java.rmi.RemoteException;
 
 
 public interface client_master_interface extends Remote {
-    Writing_request_result get_writing_slaves() throws RemoteException;
+    Writing_request_result get_writing_slaves(String filename, int filesize) throws RemoteException;
     Reading_request_result get_reading_slaves(String filename) throws RemoteException;    
 }
