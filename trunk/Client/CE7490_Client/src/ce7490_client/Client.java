bdac8f27188c652c709d30fca7e47420c9b116a3
pnquanganh1986@gmail.com <pnquanganh1986@gmail.com@81b93509-0b8c-f939-ac7f-ba0bbaeee7bc>
2012-10-01 03:10:39 +0000

    Init Client project

---
+++ trunk/Client/CE7490_Client/src/ce7490_client/Client.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ce7490_client;
+
+/**
+ *
+ * @author pham0071
+ */
+public class Client {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
