64eb5eec95587c38a990d85fae10c86841176252
pnquanganh1986 <pnquanganh1986@81b93509-0b8c-f939-ac7f-ba0bbaeee7bc>
2013-12-11 07:55:26 +0000

    Initialize project

---
+++ trunk/BuildWebGraph/src/buildwebgraph/BuildWebGraph.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buildwebgraph;
+import it.unimi.dsi.webgraph.ImmutableSequentialGraph;
+
+/**
+ *
+ * @author pham0071
+ */
+public class BuildWebGraph {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
